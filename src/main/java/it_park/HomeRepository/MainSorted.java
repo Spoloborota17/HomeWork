a4f07862d1225c3db036df2b3deb19f52bcd8e81
desktop_computer <ivan_praktika@mail.ru>
2016-10-15 16:22:16 +0300

    N-ая попытка создать репозиторий.

---
+++ src/main/java/it_park/HomeRepository/MainSorted.java
@@ -0,0 +1,34 @@
+package it_park.HomeRepository;
+
+import it_park.spoloborota.Array.SortedOne;
+import it_park.spoloborota.Array.SortedTwo;
+
+import java.util.Random;
+
+public class MainSorted {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		
+		// Создаём массив со случайным размером в диапазоне от 1 до 10000
+		int[] array = new int[random.nextInt((10000) + 1)];
+		
+		// Заполняем массив случайными целыми числами в диапазоне от 1 до размер массива*5
+		for(int i = 0; i < array.length; i++){
+			array[i] = random.nextInt(array.length * 5);
+		}
+		
+		System.out.println("***************Сортировка №1 (пузырьком)****************");
+		long begin = System.currentTimeMillis();
+		new SortedOne(array);
+		long end = System.currentTimeMillis();
+		System.out.println("Время выполнения: " + (end - begin));
+		
+		System.out.println("***************Сортировка №2 (расчёской)****************");
+		long begin2 = System.currentTimeMillis();
+		new SortedTwo(array);
+		long end2 = System.currentTimeMillis();
+		System.out.println("Время выполнения: " + (end2 - begin2));
+	}
+}
